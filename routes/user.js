778dca7facd733624772e7d8ff41d032967f767b
katayama <tattu.0310@gmail.com>
2022-05-21 16:43:41 +0900

    ✨ ミドルウェア

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,10 @@ router.get("/info", (req, res) => {
     res.send("ユーザー情報");
 });
 
+router.get("/:id", (req, res) => {
+  res.send(`${req.params.id}のお客様を取得しました`);
+});
+
 //app.listen(PORT, () => console.log(`click http://localhost:${PORT}/ !`));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const PORT = 3000;
 const userRouter = require("./routes/user");
 
+app.use(express.static("public"));
+app.use(mylogger);
+
 app.get('/', (req, res) => { 
     console.log('Hello World');
-    //res.send('Hello World');
     res.status(200).send('success');
 });
 
 app.use("/user", userRouter);
+
+//ミドルウェア
+function mylogger(req, res, next) {
+    console.log("middle",req.originalUrl);
+    next();
+}
 
 app.listen(PORT, () => console.log(`click http://localhost:${PORT}/ !`));
